c23836baf0ebbb33d732d079d88c5bddaf0591cb
Srinidhi Kaushik <srinidhikaushik@Srinidhis-MBP.lan>
2024-08-06 18:41:02 -0400

    adding invite Code

--- src/Game.tsx
+++ src/Game.tsx
@@ -1,13 +1,45 @@
 import Chessboard from './components/Chessboard/Chessboard'
 import './Game.css'
+import{ useEffect, useState } from 'react';
+import Cookies from 'js-cookie';
+
 
 function Game() {
+  const [gameType, setGameType] = useState('');
+  const [inviteCode, setInvitecode] = useState('');
+
+
+  useEffect(() => {
+    if(Cookies.get('gameType') == 'online'){
+      setGameType('online');
+      let inviteCode = Cookies.get('inviteCode');
+      if(inviteCode){
+        setInvitecode(inviteCode);
+      }
+    }
+
+  }, [gameType, inviteCode]);
+
 
   return (
     <>
-      <div>
-        <Chessboard/>
+
+      <div className="gameContainer">
+        {
+        gameType == 'online' ?
+            <div className="inviteCode">
+              <h2>Invite Code: {inviteCode}</h2>
+            </div>
+            :
+            null
+        }
+        <div className='gameBoard'>
+          <Chessboard/>
+        </div>
+
       </div>
+
+
     </>
   )
 }
--- src/Lobby.tsx
+++ src/Lobby.tsx
@@ -1,27 +1,38 @@
 import './Lobby.css'
 import TextField from '@mui/material/TextField';
+import Cookies from 'js-cookie';
 import { useNavigate } from 'react-router-dom';
 
 function Lobby() {
     const navigate = useNavigate();
+
+    function generateInviteCode() {
+        return Math.random().toString(36).substring(7).toUpperCase();
+      }
+
+    const handleSubmit = () => {
+        // Set the cookie with name 'myCookie' and value 'cookieValue2'
+        Cookies.set('gameType', 'online'); // expires in 7 days
+        Cookies.set('inviteCode', generateInviteCode()); // expires in 7 days
+        navigate("/game");
+      };
 
   return (
     <>
         <div className='title' ><h1>Online Play</h1></div>
         <div className='joinGame'>
             <div>
-                <TextField fullWidth id="outlined-basic" label="Enter Code" variant="outlined" InputLabelProps={{style : {color : 'white'} }} sx={{fieldset: { borderColor: "white" }}} />
+                <TextField fullWidth id="outlined-basic" label="Enter Code..." variant="outlined"  InputLabelProps={{style : {color : 'white'} }} sx={{fieldset: { borderColor: "white" }}}  />
             </div>
             <div>
                 <button onClick={() => navigate("/game")}> Join Game </button>
             </div>
         </div>
-
         <div>
             --- OR ---
         </div>
-        <div>
-            <button onClick={() => navigate("/game")}> Create Game</button>
+        <div className='createGame'>
+            <button onClick={handleSubmit}> Create Game</button>
         </div>
     </>
   )
